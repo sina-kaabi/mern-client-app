f694a196f88c75fef598271c078cecbb975f9a01
Sina Kaabi <kaabisina@gmail.com>
2022-05-24 00:22:36 +0100

    fixed a webpack 5 bug installation bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Nav from './Nav';
+import axios from 'axios';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
-const App = () => (
-    <div className="container pb-5">
-        <Nav />
-        <br />
-        <h1>MERN CRUD</h1>
-    </div>
-);
+const App = () => {
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = () => {
+        axios.get(`${process.env.REACT_APP_API}/posts`)
+        .then((response) => {
+            setPosts(response.data);
+        })
+        .catch();
+    };
+
+      useEffect(() => {
+        fetchPosts();
+
+     }, []);
+
+
+return(
+        <div className="container pb-5">
+            <Nav />
+            <br />
+            <h1>MERN CRUD</h1>
+        </div>
+    );
+};
+ 
 
 export default App;
